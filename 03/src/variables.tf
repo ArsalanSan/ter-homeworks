d5abe656ff30bf33d7534b5b5c3cc31f3c0c9d41
ArsalanSan <arstomorrow@gmail.com>
2023-03-29 19:44:51 +0800

    07-terraform-03-control-task2

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -39,3 +39,47 @@ variable "vpc_name" {
   default     = "develop"
   description = "VPC network&subnet name"
 }
+
+variable "vm_resources" { 
+  type = map(number)
+  default = {
+    cores         = 2,
+    memory        = 1,
+    core_fraction = 5
+  }
+  description = "Costumize VM(core, RAM, core fraction)"
+}
+
+variable "vm_platforms" {
+  type    = map(string)
+  default = {
+    v1 = "standard-v1"
+    v2 = "standart-v2"
+  }
+  description = "Platforms name"
+}
+
+variable "image_name" {
+  type    = string
+  default = "ubuntu-2004-lts"
+  description = "Image relase name"
+}
+
+variable "vms_ssh_root_key" {
+  type    = string
+  default = ""
+}
+
+variable "custum_vms" {
+  description = "Custumize VMs"
+  type = list(object(
+    {
+      name          = string
+      cores         = number
+      memory        = number
+      core_fraction = number
+      size          = number
+      type          = string
+  }))
+  default = []
+}
