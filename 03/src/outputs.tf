d1dc8ace545307c7fff47a66f6a7fc33ae5dd506
ArsalanSan <arstomorrow@gmail.com>
2023-04-02 23:16:28 +0800

    07-terraform-03-control-task5

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -1,16 +1,44 @@
 
-output "test3_output" {
+output "foreach_vms_output" {
   description = "name instance"
   value = <<EOT
+   [
     %{~ for value in yandex_compute_instance.vms ~}
+    { 
+     "name" = ${ value.name }
+     "id" = ${ value.id }
+     "fqdn" = ${ value.fqdn }
+    },
+    %{~ endfor ~}
+    %{~ for value in yandex_compute_instance.count-vm ~}
+    { 
+     "name" = ${ value.name }
+     "id" = ${ value.id }
+     "fqdn" = ${ value.fqdn}
+    },
+    %{~ endfor ~}
+    {
+     "name" = ${yandex_compute_instance.mount-disk-vm.name}
+     "id"   = ${yandex_compute_instance.mount-disk-vm.id}
+     "fqdn" = ${yandex_compute_instance.mount-disk-vm.fqdn}
+    }
+   ]
+  EOT
+}
+
+/*
+output "count_vm_output" {
+  description = "name instance"
+  value = <<EOT
+    %{~ for value in yandex_compute_instance.count-vm ~}
     "name" = ${ value.name }
-    "id" = ${ value.name }
+    "id" = ${ value.id }
     "fqdn" = ${ value.fqdn }
     %{~ endfor ~}
   EOT
 }
 
-/*
+
 output "test4_output" {
   description = "name instance"
   value =[ for i in yandex_compute_instance.vms: "'name' = '${i.name}' \n 'id' = '${i.id}' \n 'fqdn' = ${i.fqdn}"] 
